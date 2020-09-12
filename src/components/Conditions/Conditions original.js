5df4afc65e3aac876541e033a36db9e0ea21b76e
JW Nicholson <john.nicholson812@gmail.com>
2020-09-12 14:42:00 -0400

    weather display converted to mui table.Need to change background color of icon table cell

---
+++ src/components/Conditions/Conditions original.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from './Conditions.module.css';
+import { Typography } from '@material-ui/core';
+
+
+const Conditions = (props) => {
+//console.log("Unit",props.unit)
+//console.log("Humidity ", props.humidity)
+// console.log("wind speed: ", props.wind_speed)
+// console.log("wind direction ", props.wind_direction)
+
+
+    return (
+     <div >
+           {props.error && <small className={styles.error}>Enter a valid city.</small>}
+           {props.loading && <div className={styles.Loader} />}
+           {props.status === 200 ? 
+           <div>
+              <Typography variant="h4" component="h3" gutterBottom>
+               {props.name}
+              </Typography>
+                
+                {/* {props.temp && props.unit==="imperial" ? <div>YES</div> : <div>NO</div>} */}
+
+               {props.temp && props.unit==="imperial" ?
+                  <Typography>
+                    <p><strong>Temperature {Math.round(props.temp)}</strong> F&deg;</p>
+                    <p><strong>Feels like {Math.round(props.feels_like)}</strong> F&deg;</p>
+                  </Typography>
+                :
+                 <Typography>
+                    <p><strong>Temperature {Math.round(props.temp)}</strong> C&deg;</p>
+                    <p><strong>Feels like {Math.round(props.feels_like)}</strong> C&deg;</p>
+                 </Typography>
+            }
+
+              <Typography>
+                  <p><strong>Humidity</strong> {Math.round(props.humidity)}%</p>
+                  <p><strong>Wind</strong> {Math.round(props.wind_speed)}</p>
+                  <p><strong>Direction</strong> {Math.round(props.wind_direction)}&deg;</p>
+                  <p><strong>Conditions</strong> - {props.main_weather}</p>
+                  </Typography>
+      
+            <div>
+               <img src={"http://openweathermap.org/img/wn/" + props.weather_icon + "@2x.png"} alt="weather icon"/>
+               </div>
+            </div> 
+            
+            : 
+                   null
+                }
+       </div>
+  
+    )
+}
+
+export default Conditions;
+
--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,57 +1,99 @@
 import React from 'react';
 import styles from './Conditions.module.css';
-import { Typography } from '@material-ui/core';
-
+import { TableContainer, Typography, Paper, Table, TableRow, TableCell, TableBody } from '@material-ui/core';
 
 const Conditions = (props) => {
-//console.log("Unit",props.unit)
-//console.log("Humidity ", props.humidity)
-// console.log("wind speed: ", props.wind_speed)
-// console.log("wind direction ", props.wind_direction)
+	//console.log("Unit",props.unit)
+	//console.log("Humidity ", props.humidity)
+	// console.log("wind speed: ", props.wind_speed)
+	// console.log("wind direction ", props.wind_direction)
+
+	return (
+		<div>
+			{props.error && <small className={styles.error}>Enter a valid city.</small>}
+			{props.loading && <div className={styles.Loader} />}
+
+			{props.status === 200 ? (
+				//start table
+            // cheeck if there is a way to eliminate writing props.unit check twice in mui tables
+				<div id="startOutput">
+					<Typography variant="h4" component="h3" gutterBottom>
+						{props.name}
+					</Typography>
+
+					<TableContainer component={Paper}>
+						<Table aria-label="weather table">
+							<TableBody>
+								<TableRow>
+									<TableCell>Temperature</TableCell>
+									{props.unit === 'imperial' ? (
+										<TableCell align="left">{Math.round(props.temp)} F&deg;</TableCell>
+									) : (
+										<TableCell align="left">{Math.round(props.temp)} C&deg;</TableCell>
+									)}
+								</TableRow>
+
+								<TableRow>
+									<TableCell>Feels like</TableCell>
+
+									{props.unit === 'imperial' ? (
+										<TableCell align="left">{Math.round(props.feels_like)} F&deg;</TableCell>
+									) : (
+										<TableCell align="left">{Math.round(props.feels_like)} C&deg;</TableCell>
+									)}
+								</TableRow>
+
+								<TableRow>
+									<TableCell>Humidity</TableCell>
+									<TableCell align="left">{Math.round(props.humidity)}%</TableCell>
+								</TableRow>
+
+								<TableRow>
+									<TableCell>Wind</TableCell>
+
+                           {props.unit === "imperial" ?
+                              <TableCell align="left">{Math.round(props.wind_speed)}mh</TableCell>
+                            :
+                            <TableCell align="left">{Math.round(props.wind_speed)}kh</TableCell>
+                            }
+
+									
+								</TableRow>
+
+								<TableRow>
+									<TableCell>Wind direction</TableCell>
+									<TableCell align="left">{Math.round(props.wind_direction)}&deg;</TableCell>
+								</TableRow>
+
+								<TableRow>
+									<TableCell>Conditions</TableCell>
+									<TableCell align="left">{props.main_weather}</TableCell>
+								</TableRow>
+
+								
+							</TableBody>
+						</Table>
+
+                  <Table>
+                     <TableBody>
+                     <TableRow>
+									<TableCell align="center">
+										<img
+											src={'http://openweathermap.org/img/wn/' + props.weather_icon + '@2x.png'}
+											alt="weather icon"
+										/>
+									</TableCell>
+								</TableRow>
+                     </TableBody>
+                  </Table>
 
 
-    return (
-     <div >
-           {props.error && <small className={styles.error}>Enter a valid city.</small>}
-           {props.loading && <div className={styles.Loader} />}
-           {props.status === 200 ? 
-           <div>
-              <Typography variant="h4" component="h3" gutterBottom>
-               {props.name}
-              </Typography>
-                
-                {/* {props.temp && props.unit==="imperial" ? <div>YES</div> : <div>NO</div>} */}
-
-               {props.temp && props.unit==="imperial" ?
-                  <Typography>
-                    <p><strong>Temperature {Math.round(props.temp)}</strong> F&deg;</p>
-                    <p><strong>Feels like {Math.round(props.feels_like)}</strong> F&deg;</p>
-                  </Typography>
-                :
-                 <Typography>
-                    <p><strong>Temperature {Math.round(props.temp)}</strong> C&deg;</p>
-                    <p><strong>Feels like {Math.round(props.feels_like)}</strong> C&deg;</p>
-                 </Typography>
-            }
-
-              <Typography>
-                  <p><strong>Humidity</strong> {Math.round(props.humidity)}%</p>
-                  <p><strong>Wind</strong> {Math.round(props.wind_speed)}</p>
-                  <p><strong>Direction</strong> {Math.round(props.wind_direction)}&deg;</p>
-                  <p><strong>Conditions</strong> - {props.main_weather}</p>
-                  </Typography>
-               
-               <div>
-               <img src={"http://openweathermap.org/img/wn/" + props.weather_icon + "@2x.png"} alt="weather icon"/>
-               </div>
-                {/* <h6>Response Code is {props.status}</h6> */}
-            </div> : 
-                   null
-                }
-       </div>
-  
-    )
-}
+					</TableContainer>
+					{/* end table */}
+				</div>
+			) : null}
+		</div>
+	);
+};
 
 export default Conditions;
-
